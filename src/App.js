259d8adeba650026945d52bce2702ad467722de9
Raja sekhar <i.rajasekhar.cse@gmail.com>
2017-09-01 18:54:02 -0400

    feat: Add app header
    Create the header to the app with styles included

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+function App() {
+  return (
+    <div id="app">
+        <div id="app-header">
+          <span id="app-logo">My Reads</span>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    </div>
+  )
 }
+
 
 export default App;
