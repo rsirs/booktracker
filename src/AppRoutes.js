8cac3a9ec843bfc38feffd202322fd05112739fe
rsirs <>
2025-12-22 21:52:49 -0500

    feat: fix basepath

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -6,7 +6,7 @@ import SearchBooks from "./SearchBooks";
 
 function AppRoutes(props) {
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename={process.env.PUBLIC_URL ?? '/booktracker'}>
             <switch>
                 <Route exact path='/' component={App} />
                 <Route path="/search" component={SearchBooks} />
